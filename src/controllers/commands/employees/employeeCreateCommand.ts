ff31a79421a6f23602a00d6466d9ad31f1d4c44a
Tyler Spreen <tspree01@users.noreply.github.com>
2019-03-06 00:54:06 -0600

    changed the employeeEntity to allowNull true for all

--- src/controllers/commands/employees/employeeCreateCommand.ts
+++ src/controllers/commands/employees/employeeCreateCommand.ts
@@ -11,37 +11,37 @@ const validateSaveRequest = (saveEmployeeRequest: EmployeeSaveRequest): CommandR
 	const validationResponse: CommandResponse<Employee> =
 		<CommandResponse<Employee>>{ status: 200 };
 
-	if ((saveEmployeeRequest.id == null)) {
+/*	if ((saveEmployeeRequest.id == null) || isNaN(saveEmployeeRequest.id)) {
 		validationResponse.status = 421;
 		validationResponse.message = ErrorCodeLookup.EC2031;
 	} else if (saveEmployeeRequest.id < 0) {
-		validationResponse.status = 423;
+		validationResponse.status = 422;
 		validationResponse.message = ErrorCodeLookup.EC2033;
 	} else if (saveEmployeeRequest.firstName == null) {
-		validationResponse.status = 424;
+		validationResponse.status = 423;
 		validationResponse.message = ErrorCodeLookup.EC2035;
 	} else if (saveEmployeeRequest.lastName == null) {
-		validationResponse.status = 425;
+		validationResponse.status = 424;
 		validationResponse.message = ErrorCodeLookup.EC2036;
 	} else if ((saveEmployeeRequest.employeeId == null) || isNaN(saveEmployeeRequest.employeeId)) {
-		validationResponse.status = 426;
+		validationResponse.status = 425;
 		validationResponse.message = ErrorCodeLookup.EC2031;
 	} else if (saveEmployeeRequest.employeeId < 0) {
-		validationResponse.status = 427;
+		validationResponse.status = 426;
 		validationResponse.message = ErrorCodeLookup.EC2033;
 	} else if (saveEmployeeRequest.active == null) {
-		validationResponse.status = 428;
+		validationResponse.status = 427;
 		validationResponse.message = ErrorCodeLookup.EC2032;
 	} else if ((saveEmployeeRequest.manager == null ) || isNaN(saveEmployeeRequest.employeeId)) {
-		validationResponse.status = 429;
+		validationResponse.status = 428;
 		validationResponse.message = ErrorCodeLookup.EC2034;
 	} else if (saveEmployeeRequest.manager < 0) {
-		validationResponse.status = 430;
+		validationResponse.status = 429;
 		validationResponse.message = ErrorCodeLookup.EC2035;
 	} else if ((saveEmployeeRequest.password == null) || (saveEmployeeRequest.password.trim() === "")) {
-		validationResponse.status = 431;
+		validationResponse.status = 430;
 		validationResponse.message = ErrorCodeLookup.EC2038;
-	}
+	}*/
 
 	return validationResponse;
 };
