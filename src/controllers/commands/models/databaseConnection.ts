d322adfdefc2c849631794bc69cc4250f7164377
Tyler Spreen <tspree01@users.noreply.github.com>
2020-02-15 16:56:00 -0600

    changed the databaseConnection.ts from postgre to MySQL

--- src/controllers/commands/models/databaseConnection.ts
+++ src/controllers/commands/models/databaseConnection.ts
@@ -7,8 +7,8 @@ export const DatabaseConnection: Sequelize.Sequelize =
 	new Sequelize(
 		<string>process.env.DATABASE_URL,
 		<Sequelize.Options>{
-			dialect:  "MySQL",
-			protocol: "MySQL",
+			dialect:  "mysql",
+			protocol: "mysql",
 			omitNull: true,
 			freezeTableName: true,
 			pool: <Sequelize.PoolOptions>{
